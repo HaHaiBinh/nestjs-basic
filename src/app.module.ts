a0a4e0dde1ee3e000e11047ee3f18435e159586a
Ha Hai Binh <hhbinh@ppj-international.com>
2026-08-06 09:53:19 +0700

    connect db mongo video 18

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://hahaibinh:vOANCi3RuVylyPDw@cluster0.icttyy8.mongodb.net',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
