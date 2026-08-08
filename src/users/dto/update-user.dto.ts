1e6192c6251907d25a315068605c7f79e3e0de56
Ha Hai Binh <hhbinh@ppj-international.com>
2026-08-08 10:34:07 +0700

    update and delete video 25-26

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsNotEmpty, IsEmail } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    @IsEmail({}, { message: 'Invalid email format' })
+    @IsNotEmpty({ message: 'Email is required' })
+    email: string;
+
+    @IsNotEmpty({ message: 'Password is required' })
+    password: string;
+
+    name: string;
+    address: string;
+}
