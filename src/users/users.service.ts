e4a16a1c591e36e43a082aeaa30126a431a7f23a
Ha Hai Binh <hhbinh@ppj-international.com>
2026-08-08 09:30:45 +0700

    hash user password video 22 part 2

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,14 +4,26 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
+import { genSaltSync, hashSync } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
+  hashPassword = (password: string) => {
+    const salt = genSaltSync(10);
+    const hash = hashSync(password, salt);
+    return hash;
+  };
+
   // create(createUserDto: CreateUserDto) {
   async create(email: string, password: string, name: string) {
-    const newUser = new this.userModel({ email, password, name });
+    const hashedPassword = this.hashPassword(password);
+    const newUser = new this.userModel({
+      email,
+      password: hashedPassword,
+      name,
+    });
     return await newUser.save();
   }
 
